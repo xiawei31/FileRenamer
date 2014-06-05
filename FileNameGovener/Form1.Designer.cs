29f198feb3542af1026cd7b65447d5265629010d
Parthas-Menethil <xywhpanda@gmail.com>
2014-06-05 00:06:13 -0400

    Initial Commit

---
+++ FileNameGovener/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace FileNameGovener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.btnRenameFiles = new System.Windows.Forms.Button();
+            this.chkReverse = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrefix = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkStepTwo = new System.Windows.Forms.CheckBox();
+            this.lstDesignated = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(13, 45);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(301, 160);
+            this.lstFiles.TabIndex = 0;
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(188, 216);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(101, 37);
+            this.btnOpenFolder.TabIndex = 1;
+            this.btnOpenFolder.Text = "Open Folder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // btnRenameFiles
+            // 
+            this.btnRenameFiles.Location = new System.Drawing.Point(465, 216);
+            this.btnRenameFiles.Name = "btnRenameFiles";
+            this.btnRenameFiles.Size = new System.Drawing.Size(101, 37);
+            this.btnRenameFiles.TabIndex = 2;
+            this.btnRenameFiles.Text = "Rename Files";
+            this.btnRenameFiles.UseVisualStyleBackColor = true;
+            this.btnRenameFiles.Click += new System.EventHandler(this.btnRenameFiles_Click);
+            // 
+            // chkReverse
+            // 
+            this.chkReverse.AutoSize = true;
+            this.chkReverse.Location = new System.Drawing.Point(59, 264);
+            this.chkReverse.Name = "chkReverse";
+            this.chkReverse.Size = new System.Drawing.Size(80, 17);
+            this.chkReverse.TabIndex = 3;
+            this.chkReverse.Text = "Reverse All";
+            this.chkReverse.UseVisualStyleBackColor = true;
+            this.chkReverse.CheckedChanged += new System.EventHandler(this.chkReverse_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Prefix:";
+            // 
+            // txtPrefix
+            // 
+            this.txtPrefix.Location = new System.Drawing.Point(59, 211);
+            this.txtPrefix.Name = "txtPrefix";
+            this.txtPrefix.Size = new System.Drawing.Size(100, 20);
+            this.txtPrefix.TabIndex = 5;
+            this.txtPrefix.Text = "file_";
+            this.txtPrefix.TextChanged += new System.EventHandler(this.txtPrefix_TextChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(59, 238);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Start:";
+            // 
+            // chkStepTwo
+            // 
+            this.chkStepTwo.AutoSize = true;
+            this.chkStepTwo.Checked = true;
+            this.chkStepTwo.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkStepTwo.Location = new System.Drawing.Point(156, 264);
+            this.chkStepTwo.Name = "chkStepTwo";
+            this.chkStepTwo.Size = new System.Drawing.Size(57, 17);
+            this.chkStepTwo.TabIndex = 8;
+            this.chkStepTwo.Text = "Step 2";
+            this.chkStepTwo.UseVisualStyleBackColor = true;
+            this.chkStepTwo.CheckedChanged += new System.EventHandler(this.chkStepTwo_CheckedChanged);
+            // 
+            // lstDesignated
+            // 
+            this.lstDesignated.FormattingEnabled = true;
+            this.lstDesignated.Location = new System.Drawing.Point(352, 44);
+            this.lstDesignated.Name = "lstDesignated";
+            this.lstDesignated.Size = new System.Drawing.Size(214, 160);
+            this.lstDesignated.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(321, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "=>";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "FileList:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(349, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Target Preview:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 293);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstDesignated);
+            this.Controls.Add(this.chkStepTwo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.txtPrefix);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkReverse);
+            this.Controls.Add(this.btnRenameFiles);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.lstFiles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.Button btnRenameFiles;
+        private System.Windows.Forms.CheckBox chkReverse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrefix;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkStepTwo;
+        private System.Windows.Forms.ListBox lstDesignated;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ FileNameGovener/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+namespace FileNameGovener
+{
+    public partial class Form1 : Form
+    {
+        string path = "";
+        List<string> GFiles = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = folderBrowserDialog.SelectedPath;
+                LoadDir();
+            }
+            CalculateDesinatedList();
+        }
+        private void LoadDir()
+        {
+            GFiles.Clear();
+            if (path != "" && Directory.Exists(path))
+            {
+                string[] lFiles = Directory.GetFiles(path);
+                AlphanumComparator ns = new AlphanumComparator();
+                string temp = "";
+                for (int write = 0; write < lFiles.Length; write++)
+                {
+                    for (int sort = 0; sort < lFiles.Length - 1; sort++)
+                    {
+                        if (ns.Compare(lFiles[sort], lFiles[sort + 1]) > 0)
+                        {
+                            temp = lFiles[sort + 1];
+                            lFiles[sort + 1] = lFiles[sort];
+                            lFiles[sort] = temp;
+                        }
+                    }
+                }
+                GFiles.AddRange(lFiles);
+                UpdateList();
+            }
+        }
+        private void UpdateList()
+        {
+            lstFiles.Items.Clear();
+            lstFiles.Items.AddRange(GFiles.ToArray());
+        }
+        private void chkReverse_CheckedChanged(object sender, EventArgs e)
+        {
+            if (GFiles.Count > 0)
+            {
+                GFiles.Reverse();
+                UpdateList();
+            }
+            else
+            {
+                chkReverse.Checked = false;
+            }
+            CalculateDesinatedList();
+        }
+        private static Random random = new Random((int)DateTime.Now.Ticks);//thanks to McAden
+        private string RandomString(int size)
+        {
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
+        }
+
+        private void btnRenameFiles_Click(object sender, EventArgs e)
+        {
+            if (GFiles.Count > 0)
+            {
+                string lNewFileName = RandomString(10);
+                string lNewAbsolutePath = Path.Combine(path, lNewFileName);
+                int lBaseNum = int.Parse(numericUpDown1.Value.ToString());
+                Directory.CreateDirectory(lNewAbsolutePath);
+                string lExtension = "";
+                foreach (string str in GFiles)
+                {
+                    lExtension = Path.GetExtension(str);
+                    File.Copy(str, Path.Combine(lNewAbsolutePath, txtPrefix.Text + (lBaseNum++) + lExtension));
+                    if (chkStepTwo.Checked) {
+                        lBaseNum++;
+                    }
+                }
+                MessageBox.Show("Done!");
+                System.Diagnostics.Process.Start("explorer.exe",lNewAbsolutePath);
+            }
+            else {
+                MessageBox.Show("No file to process.");
+            }
+        }
+        private void CalculateDesinatedList() {
+            if (GFiles.Count > 0)
+            {
+                lstDesignated.Items.Clear();
+                int lBaseNum = int.Parse(numericUpDown1.Value.ToString());
+                string lExtension = "";
+                foreach (string str in GFiles)
+                {
+                    lExtension = Path.GetExtension(str);
+                    lstDesignated.Items.Add(txtPrefix.Text + (lBaseNum++) + lExtension);
+                    if (chkStepTwo.Checked)
+                    {
+                        lBaseNum++;
+                    }
+                }
+            }
+        }
+
+        private void chkStepTwo_CheckedChanged(object sender, EventArgs e)
+        {
+            CalculateDesinatedList();
+        }
+
+        private void txtPrefix_TextChanged(object sender, EventArgs e)
+        {
+            CalculateDesinatedList();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            CalculateDesinatedList();
+        }
+    }
+}
